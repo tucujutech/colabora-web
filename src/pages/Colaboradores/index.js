f27c404f852d1a485a38570a7cea5487ad3c0cf2
alexleao23 <alexandersilvadeleao23@gmail.com>
2019-09-02 18:00:02 -0300

    Ajuste para mostrar cards na lista de colaboradores

--- src/pages/Colaboradores/index.js
+++ src/pages/Colaboradores/index.js
@@ -1,94 +1,189 @@
 import React, { Component } from 'react';
 import './styles.css';
+import Juvia from '../../assets/images/juvia.jpg';
 
 class Colaboradores extends Component {
   render() {
     return (
       <div className="Colaboradores">
-        <div className="page-header">
-          <h3>Lista de Colaboradores</h3>
+        <div className="colaborador-card">
+          <div className="colab-card-content">
+            <img src={Juvia} alt="Juvia"/>
+            <div className="colab-info">
+              <span>Nome: </span>
+              Alexander Silva de Leão
+              <br/>
+              <span>Função: </span>
+              Desenvolvedor FullStack
+              <br/>
+              <span>Entrada: </span>
+              02/09/2019 17:30
+            </div>
+          </div>
+          <div className="colab-card-footer">
+            <button type="button">
+              <i className="fas fa-eye"></i>
+              Ver
+            </button>
+            <button type="button">
+              <i className="fas fa-edit"></i>
+              Editar
+            </button>
+            <button type="button">
+              <i className="fas fa-trash"></i>
+              Deletar
+            </button>
+          </div>
         </div>
-        <div className="table-bordered">
-          <table>
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>Especialidade</th>
-                <th>Data de Criação</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  Alexander Silva de Leão
-                </td>
-                <td>Desenvolvedor FullStack</td>
-                <td>
-                  26/08/2019 13:35
-                </td>
-                <td>
-                  <button type="button">
-                    <i className="fas fa-eye"></i>
-                  </button>
-                  <button type="button">
-                    <i className="fas fa-edit"></i>
-                  </button>
-                  <button type="button">
-                    <i className="fas fa-trash"></i>
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  Philipe Huan Ramos
-                </td>
-                <td>
-                  Desenvolvedor Backend
-                </td>
-                <td>
-                  26/08/2019 13:35
-                </td>
-                <td>
-                  <button type="button">
-                    <i className="fas fa-eye"></i>
-                  </button>
-                  <button type="button">
-                    <i className="fas fa-edit"></i>
-                  </button>
-                  <button type="button">
-                    <i className="fas fa-trash"></i>
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>Lucas Figueiredo</td>
-                <td>Desenvolvedor Frontend</td>
-                <td>
-                  26/08/2019 13:35
-                </td>
-                <td>
-                  <button type="button">
-                    <i className="fas fa-eye"></i>
-                  </button>
-                  <button type="button">
-                    <i className="fas fa-edit"></i>
-                  </button>
-                  <button type="button">
-                    <i className="fas fa-trash"></i>
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-            <tfoot>
-              <tr>
-                <th>Nome</th>
-                <th>Especialidade</th>
-                <th>Data de Criação</th>
-                <th>Ações</th>
-              </tr>
-            </tfoot>
-          </table>
+
+        <div className="colaborador-card">
+          <div className="colab-card-content">
+            <img src={Juvia} alt="Juvia"/>
+            <div className="colab-info">
+              <span>Nome: </span>
+              Lucas Figueiredo
+              <br />
+              <span>Função: </span>
+              Desenvolvedor Frontend
+              <br />
+              <span>Entrada: </span>
+              02/09/2019 17:30
+            </div>
+          </div>
+          <div className="colab-card-footer">
+            <button type="button">
+              <i className="fas fa-eye"></i>
+              Ver
+            </button>
+            <button type="button">
+              <i className="fas fa-edit"></i>
+              Editar
+            </button>
+            <button type="button">
+              <i className="fas fa-trash"></i>
+              Deletar
+            </button>
+          </div>
+        </div>
+
+        <div className="colaborador-card">
+          <div className="colab-card-content">
+            <img src={Juvia} alt="Juvia"/>
+            <div className="colab-info">
+              <span>Nome: </span>
+              Philipe Huan Ramos
+              <br />
+              <span>Função: </span>
+              Desenvolvedor Backend
+              <br />
+              <span>Entrada: </span>
+              02/09/2019 17:30
+            </div>
+          </div>
+          <div className="colab-card-footer">
+            <button type="button">
+              <i className="fas fa-eye"></i>
+              Ver
+            </button>
+            <button type="button">
+              <i className="fas fa-edit"></i>
+              Editar
+            </button>
+            <button type="button">
+              <i className="fas fa-trash"></i>
+              Deletar
+            </button>
+          </div>
+        </div>
+        
+        <div className="colaborador-card">
+          <div className="colab-card-content">
+            <img src={Juvia} alt="Juvia"/>
+            <div className="colab-info">
+              <span>Nome: </span>
+              Alexander Silva de Leão
+              <br/>
+              <span>Função: </span>
+              Desenvolvedor FullStack
+              <br/>
+              <span>Entrada: </span>
+              02/09/2019 17:30
+            </div>
+          </div>
+          <div className="colab-card-footer">
+            <button type="button">
+              <i className="fas fa-eye"></i>
+              Ver
+            </button>
+            <button type="button">
+              <i className="fas fa-edit"></i>
+              Editar
+            </button>
+            <button type="button">
+              <i className="fas fa-trash"></i>
+              Deletar
+            </button>
+          </div>
+        </div>
+
+        <div className="colaborador-card">
+          <div className="colab-card-content">
+            <img src={Juvia} alt="Juvia"/>
+            <div className="colab-info">
+              <span>Nome: </span>
+              Lucas Figueiredo
+              <br />
+              <span>Função: </span>
+              Desenvolvedor Frontend
+              <br />
+              <span>Entrada: </span>
+              02/09/2019 17:30
+            </div>
+          </div>
+          <div className="colab-card-footer">
+            <button type="button">
+              <i className="fas fa-eye"></i>
+              Ver
+            </button>
+            <button type="button">
+              <i className="fas fa-edit"></i>
+              Editar
+            </button>
+            <button type="button">
+              <i className="fas fa-trash"></i>
+              Deletar
+            </button>
+          </div>
+        </div>
+
+        <div className="colaborador-card">
+          <div className="colab-card-content">
+            <img src={Juvia} alt="Juvia"/>
+            <div className="colab-info">
+              <span>Nome: </span>
+              Philipe Huan Ramos
+              <br />
+              <span>Função: </span>
+              Desenvolvedor Backend
+              <br />
+              <span>Entrada: </span>
+              02/09/2019 17:30
+            </div>
+          </div>
+          <div className="colab-card-footer">
+            <button type="button">
+              <i className="fas fa-eye"></i>
+              Ver
+            </button>
+            <button type="button">
+              <i className="fas fa-edit"></i>
+              Editar
+            </button>
+            <button type="button">
+              <i className="fas fa-trash"></i>
+              Deletar
+            </button>
+          </div>
         </div>
       </div>
     );
